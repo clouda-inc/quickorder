1fcbb8c469cb6081c80ce96fdcde4e0d46eea722
Sahan Jayawardana <sahan@clouda.io>
2022-02-09 12:44:05 +0530

    Add timestamp to logs
    Reference: SSS-444

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Fixed
+
+- Added timestamp to logs
+
 ## [0.1.9] - 2022-02-08
 
 ### Fixed
--- node/middlewares/health.ts
+++ node/middlewares/health.ts
@@ -1,11 +1,19 @@
+import moment from "moment"
+
 export async function health(ctx: Context, next: () => Promise<any>) {
   const {
     vtex: { logger },
+    clients: { masterdata: mdClient }
   } = ctx
 
   // The scheduler "bulk-order-health-check" will be using
   // this end-point to ensure that this service is up and running.
-  logger.info('Bulk order service is up and running.')
+  const timeLiteral = moment().format('YYYY-MM-DD HH:mm:ss.SSSSSSSSS')
+  logger.info(`Bulk order service is up and running at ${timeLiteral}.`)
+  await mdClient.createDocument({
+    dataEntity: 'TestServiceLog',
+    fields: { timeLiteral },
+  })
 
   ctx.status = 200
   ctx.set('Cache-Control', 'No-Cache')
